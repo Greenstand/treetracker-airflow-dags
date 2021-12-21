b931152c2e395856b19bddad42819f8f388c4316
deanchen <antlet2004@gmail.com>
2021-12-21 14:33:57 +0800

    feat: be able to run with month param, check exisiting

--- lib/capture_export_test.py
+++ lib/capture_export_test.py
@@ -11,7 +11,13 @@ class Test_TestIncrementDecrement(unittest.TestCase):
         DB_URL = os.environ['DB_URL']
         print("DB_URL:", DB_URL)
         conn = psycopg2.connect(DB_URL, sslmode='require')
-        self.assertEqual(capture_export.capture_export(conn, '2020-12'), "Done")
+        # execute capture_export.capture_export(conn, '2020-12') should throw an error
+        try:
+            capture_export.capture_export(conn, '2020-11')
+            self.assertTrue(False)
+        except Exception as e:
+            print("get error when exec SQL:", e)
+            self.assertTrue(True)
 
 if __name__ == '__main__':
     # Run the unit tests in the test suite with name 'Test_TestIncrementDecrement'
