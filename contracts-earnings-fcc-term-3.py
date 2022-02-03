6fa9c643e98dc3fda666d7a614747a7bd34a795c
Zaven Arra <matthew.fullstackgis@gmail.com>
2022-02-03 10:47:50 -0800

    fix: update identifier

--- contracts-earnings-fcc-term-3.py
+++ contracts-earnings-fcc-term-3.py
@@ -33,7 +33,7 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 with DAG(
-    'contract-earnings-fcc-term-2',
+    'contract-earnings-fcc-term-3',
     default_args=default_args,
     description='Calculate earnings for FCC planters',
     schedule_interval= None,
