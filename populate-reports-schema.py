3179087511f1503f3d05f55f1f412ae9bc2b3f9c
Dadiorchen <dadiorchen@outlook.com>
2022-10-25 11:35:26 +0800

    Revert "fix: remove restrictions"

--- populate-reports-schema.py
+++ populate-reports-schema.py
@@ -83,12 +83,12 @@ with DAG(
                 FROM region
                 JOIN region_type
                 ON region_type.id = region.type_id
-                --- WHERE region_type.type = 'fcc_catchments'
+                WHERE region_type.type = 'fcc_catchments'
               ) region
               ON ST_WITHIN(trees.estimated_geometric_location, region.geom) 
               WHERE trees.active = true
               AND planter_identifier IS NOT NULL
-              --- AND planter.organization_id IN (SELECT entity_id from getEntityRelationshipChildren(178))
+              AND planter.organization_id IN (SELECT entity_id from getEntityRelationshipChildren(178))
               --- AND trees.id = 827280 
               ;
             """);
