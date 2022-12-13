98a14f1bdb1a7a4d4c79480d8166cbbf7234c0a6
Olojakpoke Daniel <holodja@hotmail.com>
2022-12-13 18:00:48 +0100

    dag fix

--- migrate_devices_dag.py
+++ migrate_devices_dag.py
@@ -19,6 +19,7 @@ default_args = {
     'email_on_retry': False,
     'retries': 2,
     'retry_delay': timedelta(minutes=5),
+    'start_date'=days_ago(7),
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
@@ -38,7 +39,6 @@ with DAG(
     default_args=default_args,
     description='Migrate devices from legacy db',
     schedule_interval= None,
-    start_date=days_ago(7),
     catchup=False,
     tags=['migration'],
 ) as dag:
