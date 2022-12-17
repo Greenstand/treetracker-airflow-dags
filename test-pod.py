d2ddc658f90fbae366624a3a7e7e4c53ffaac39a
deanchen <antlet2004@gmail.com>
2022-12-17 11:36:24 +0800

    fix: pod run

--- test-pod.py
+++ test-pod.py
@@ -50,21 +50,17 @@ with DAG(
 
     postgresConnId = "postgres_default"
 
-    write_xcom = KubernetesPodOperator(
+    podrun = KubernetesPodOperator(
         namespace="default",
         image="alpine",
-        cmds=["sh", "-c", "mkdir -p /airflow/xcom/;echo '[1,2,3,4]' > /airflow/xcom/return.json"],
-        name="write-xcom",
+        cmds=["sh", "-c", "echo 'ok!'"],
+        name="run k8s pod command",
         do_xcom_push=True,
         is_delete_operator_pod=True,
         in_cluster=True,
-        task_id="write-xcom",
+        task_id="k8s-pod",
         get_logs=True,
     )
 
-    pod_task_xcom_result = BashOperator(
-        bash_command="echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\"",
-        task_id="pod_task_xcom_result",
-    )
 
-    write_xcom >> pod_task_xcom_result
+    podrun >> t1
