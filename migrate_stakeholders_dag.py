e5b238bbede77b4c40cfb60799403e017502adf1
Huang Pan <huangpan@yahoo.com>
2023-02-06 13:41:52 -0800

    added password masking to DB_URL. Tested working on local Airflow

--- migrate_stakeholders_dag.py
+++ migrate_stakeholders_dag.py
@@ -55,7 +55,7 @@ with DAG(
     conn = db.get_uri()
     # Airflow Variable DATABASE_PASSWORD is masked in the Airflow Variables UI and logs but not when actually used
     # https://airflow.apache.org/docs/apache-airflow/stable/administration-and-deployment/security/secrets/mask-sensitive-values.html
-    # tested working in the test1.py DAG
+    # tested working in the https://github.com/Greenstand/treetracker-airflow-dags/blob/main/test1.py DAG
     environments = {
         'DATABASE_URL': "postgresql://" + Variable.get("DATABASE_LOGIN") + ":" + Variable.get("DATABASE_PASSWORD") + "@" + Variable.get("DATABASE"),
         'NODE_TLS_REJECT_UNAUTHORIZED': '0',
